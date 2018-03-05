9464012bca0113cc702d5f644cb4ebe15244874d
Katelyn Gadd <kg@luminance.org>
2018-03-04 18:52:36 -0800

    Generate extension ID based on current windows user SID Add (currently empty) native messaging host manifest to chrome registry config Customize help page to point to installed extension

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace Viramate {
+    class Program {
+        static void Main (string[] args) {
+            if (args.Length == 0)
+                InstallExtension();
+        }
+
+        static string ExtensionId {
+            get {
+                var identity = WindowsIdentity.GetCurrent();
+                var binLength = identity.User.BinaryLength;
+                var bin = new byte[binLength];
+                identity.User.GetBinaryForm(bin, 0);
+                Array.Reverse(bin);
+
+                var result = new char[32];
+                Array.Copy("viramate".ToCharArray(), result, 8);
+                for (int i = 0; i < binLength; i++) {
+                    int j = i + 8;
+                    if (j >= result.Length)
+                        break;
+                    result[j] = (char)((int)'a' + (bin[i] % 26));
+                }
+                return new string(result);
+            }
+        }
+
+        static string InstallPath {
+            get {
+                var folder = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                return Path.Combine(folder, "Viramate");
+            }
+        }
+
+        static string ExecutablePath {
+            get {
+                var cb = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+                var uri = new Uri(cb);
+                return Path.GetDirectoryName(uri.LocalPath);
+            }
+        }
+
+        static void InstallExtension () {
+            Console.WriteLine("Installing extension. This'll take a moment...");
+
+            Console.WriteLine($"Extension id: {ExtensionId}");
+
+            const string keyName = @"Software\Google\Chrome\NativeMessagingHosts\viramate";
+            using (var key = Registry.CurrentUser.CreateSubKey(keyName, true)) {
+                var manifestPath = Path.Combine(ExecutablePath, "nmh.json");
+
+                Console.WriteLine($"{keyName}\\@ = {manifestPath}");
+                key.SetValue(null, manifestPath);
+            }
+
+            var helpFilePath = Path.Combine(ExecutablePath, "Help", "index.html");
+            var text = File.ReadAllText(helpFilePath);
+            text = Regex.Replace(text, @"\<pre\ id='extension_path'>[^<]*\</pre\>", @"<pre id='extension_path'>" + InstallPath + "</pre>");
+            File.WriteAllText(helpFilePath, text);
+
+            Console.WriteLine("Viramate has been downloaded. Opening install instructions...");
+            Process.Start(helpFilePath);
+            Console.WriteLine("Press enter to exit.");
+            Console.ReadLine();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# ViramateInstaller
-Viramate installer &amp; updater
+This program installs and automatically updates Viramate. The extension communicates with it using Chrome Native Messaging, so it will be running in the background while the extension is in use.
+
+This program is not released under any open source or public domain license - redistribution (especially of modified versions) is prohibited with the specific exception of forks where you are preparing a pull request. You're welcome to use a modified version for your own personal use if you like, and if you'd like to use this for an extension of your own, let me know and we can work something out. This is to ensure users of the extension get updates when something important changes in the game.
+
+To compile it yourself you'll need Visual Studio 2017. The community version is enough. (OS X and Linux versions are probably possible using Mono or .NET core, but I haven't looked into that. Feel free to file a pull request if you come up with something.)
